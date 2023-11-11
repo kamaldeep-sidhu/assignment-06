033b4600713104a1e8630dee4023beb50e9f3def
kamaldeep-sidhu <kamaldeepkaur2@academic.rrc.ca>
2023-11-11 13:15:42 -0600

    this step includes importing of enumerations and defining of mortgage class:Set up project directory and file structure

--- sdf_assignment_6/assignment_6_starter/mortgage/mortgage.py
+++ sdf_assignment_6/assignment_6_starter/mortgage/mortgage.py
@@ -1,9 +1,14 @@
 """
 Description: A class meant to manage Mortgage options.
-Author: {Student Name}
-Date: {Date}
+Author: {kamaldeep kaur }
+Date: {11-11-2023}
 Usage: Create an instance of the Mortgage class to manage mortgage records and 
 calculate payments.
 """
+from mortgage.pixell_lookup import MortgageRate, MortgageFrequency, VALID_AMORTIZATION
+class Mortgage:
+
+                  
+                    
 
 
